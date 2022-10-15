e1482fd98b56f2e62fa459c2d32883bf3a2026d4
miss <ddc.akari@gmail.com>
2022-10-16 06:06:10 +0900

    save

---
+++ main.js
@@ -0,0 +1,31 @@
+// 배너 이미지 슬라이드
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// 영화차트 탭 메뉴
+
+const movBtn = $(".movieTitle > ul > li");
+const movCont = $(".movieChart>div");
+
+movCont.hide().eq(0).show();
+
+movBtn.click(function (e) {
+  e.preventDefault();
+  const target = $(this);
+  const index = target.index();
+  // alert(index);
+  movBtn.removeClass("active");
+  target.addClass("active");
+  movCont.css("display", "none");
+  movCont.eq(index).css("display", "block");
+});
