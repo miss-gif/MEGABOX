56ea20235001b927b3ce429f15142b2b09d86c79
miss <ddc.akari@gmail.com>
2022-10-17 22:28:55 +0900

    save

--- main.js
+++ main.js
@@ -16,6 +16,23 @@ var swiper = new Swiper(".swiper-container", {
 var swiper = new Swiper(".swiper-container2", {
   slidesPerView: 4, // 한 번에 보여줄 슬라이드 개수
   spaceBetween: 24,
+  breakpoints: {
+    // when window width is >= 600px
+    600: {
+      slidesPerView: 1.4,
+      spaceBetween: 24,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    // when window width is >= 960px
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
 });
 
 // 영화차트 탭 메뉴
